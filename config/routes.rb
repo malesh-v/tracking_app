db1641763222f1154219428029be853f4ee4d202
Vadim <gam.vadik@gmail.com>
2018-06-27 16:04:27 +0300

    test

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :staff_members, only: [:new, :create, :edit, :update, :destroy]
   resources :sessions,      only: [:new, :create, :destroy]
-  resources :statuses,      only: [:new, :create, :edit, :update, :destroy]
-  
+  resources :statuses
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
