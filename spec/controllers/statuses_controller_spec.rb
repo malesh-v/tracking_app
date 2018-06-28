79b88c44f9ec7ece8835fa48b29bbe25569b7561
Vadim <gam.vadik@gmail.com>
2018-06-28 20:01:48 +0300

    statuses test finish

--- spec/controllers/statuses_controller_spec.rb
+++ spec/controllers/statuses_controller_spec.rb
@@ -64,53 +64,34 @@ describe StatusesController do
     end
   end
 
+  describe 'logged as admin' do
 
+    let(:admin) { FactoryGirl.create(:admin) }
+
+    before do
+      sign_in admin, no_capybara: true
+    end
+
+    describe 'GET #new' do
+      it 'should create' do
+        post :create, params: { status: { name: name} }
+        Status.find_by_name(name).should_not be_nil
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'edit the requested status' do
+        put :update, params: {id: status.id, status: { name: name} }
+        status.reload.name.should eq(name)
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'destroys the requested status' do
+        expect {
+          delete :destroy, params: {id: status.id }
+        }.to change(Status, :count).by(-1)
+      end
+    end
+  end
 end
-
-=begin
-  describe 'PUT #update' do
-    context 'with valid params' do
-      let(:new_attributes) {
-        skip('Add a hash of attributes valid for your model')
-      }
-
-      it 'updates the requested status' do
-        status = Status.create! valid_attributes
-        put :update, params: {id: status.to_param, status: new_attributes}, session: valid_session
-        status.reload
-        skip('Add assertions for updated state')
-      end
-
-      it 'redirects to the status' do
-        status = Status.create! valid_attributes
-        put :update, params: {id: status.to_param, status: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(status)
-      end
-    end
-
-    context 'with invalid params' do
-      it 'returns a success response (i.e. to display the edit template)' do
-        status = Status.create! valid_attributes
-        put :update, params: {id: status.to_param, status: invalid_attributes}, session: valid_session
-        expect(response).to be_success
-      end
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    it 'destroys the requested status' do
-      status = Status.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: status.to_param}, session: valid_session
-      }.to change(Status, :count).by(-1)
-    end
-
-    it 'redirects to the statuses list' do
-      status = Status.create! valid_attributes
-      delete :destroy, params: {id: status.to_param}, session: valid_session
-      expect(response).to redirect_to(statuses_url)
-    end
-  end
-=end
-
-
