3fa47891edff707ca6eb27d8ba5de0eef0f81bb4
Vadim <gam.vadik@gmail.com>
2018-07-17 12:34:45 +0300

    activity_with_test_improve

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -5,19 +5,30 @@ class ActivityLog < ApplicationRecord
 
     message = ''
     if update_params[:status_id].to_i != ticket.status_id
-      message += ' changed status to ' + Status.find_by_id(update_params[:status_id]).name + ','
+      message += message_for_item('status', update_params[:status_id])
     end
     if update_params[:staff_member_id] != ticket.staff_member_id.to_s
-      if update_params[:staff_member_id] == ''
-        message += ' changed ticket owner to Unassigned,'
-      else
-        message += ' assigned to ' + StaffMember.find_by_id(update_params[:staff_member_id]).login + ','
-      end
+      message += message_for_item('staffmember', update_params[:staff_member_id])
     end
     if update_params[:department_id].to_i != ticket.department_id
-      message += ' changed department to ' + Department.find_by_id(update_params[:department_id]).name + ','
+      message += message_for_item('department', update_params[:department_id])
     end
 
     message.chomp!(',')
+  end
+
+  def self.message_for_item(model, id)
+    case model
+    when 'status'
+      ' changed status to ' + Status.find_by_id(id).name + ','
+    when 'department'
+      ' changed department to ' + Department.find_by_id(id).name + ','
+    else
+      if id == ''
+        ' changed ticket owner to Unassigned,'
+      else
+        ' assigned ticket to ' + StaffMember.find_by_id(id).login + ','
+      end
+    end
   end
 end
