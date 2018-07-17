08ab84755c1739dc2337db12eb12a902112f5cc7
Vadim <gam.vadik@gmail.com>
2018-07-17 23:20:07 +0300

    test for search

--- spec/helpers/tickets_helper.rb
+++ spec/helpers/tickets_helper.rb
@@ -4,11 +4,14 @@ require 'support/utilities'
 shared_context 'tickets page' do
   describe 'GET statuses list' do
 
-    specify do
-      click_button 'Search'
+    before { click_button 'Search' }
 
+    specify 'observe link with term' do
       url_with_term = tickets_path(term: "#{cookies[:term]}")
-      find(:xpath, "//a[@href='/tickets?no_term=true']")
+      url_clear_term = tickets_path(no_term: 'true')
+
+      assert_selector("a[href='#{url_clear_term}']", text: 'All Tickets', count: 1)
+
       #tickets path
       expect(current_path).to eq tickets_path
       assert_selector('li.list-group-item > a.show_ticket', count: 2)
@@ -23,27 +26,26 @@ shared_context 'tickets page' do
       #edit ticket path
       visit edit_ticket_path(Ticket.first)
       assert_selector("a[href='#{url_with_term}']", text: 'Back', count: 1)
+
+      find(:xpath, "//a[@href='/tickets?no_term=true']")
     end
-    specify do
-      url_with_term = tickets_path(term: "#{cookies[:term]}")
-      click_button 'Search'
+    specify 'search and clear term' do
+      url_clear_term = tickets_path(no_term: 'true')
 
-      find(:xpath, "//a[@href='/tickets?no_term=true']")
+      assert_selector("a[href='#{url_clear_term}']", text: 'All Tickets', count: 1)
+      find(:xpath, "//a[@href='/tickets?no_term=true']").click
 
-      #tickets path
-      expect(current_path).to eq tickets_path
-      assert_selector('li.list-group-item > a.show_ticket', count: 2)
-      assert_selector('a.badge', text: 'Edit',              count: 2)
-      assert_selector('li.filter > a.list-group-item',      count: 0)
-      assert_selector('div#header-tickets > h1', text: 'Tickets list', count: 1)
+      #clear term from cookies
+      assert_selector('li.filter > a.list-group-item', count: 4)
+      assert_selector("a[href='#{tickets_path}']", text: 'All Tickets', count: 1)
 
       #show ticket parh
       visit ticket_path(Ticket.first)
-      assert_selector("a[href='#{url_with_term}']", text: 'Back', count: 1)
+      assert_selector("a[href='#{tickets_path}']", text: 'Back', count: 1)
 
       #edit ticket path
       visit edit_ticket_path(Ticket.first)
-      assert_selector("a[href='#{url_with_term}']", text: 'Back', count: 1)
+      assert_selector("a[href='#{tickets_path}']", text: 'Back', count: 1)
     end
   end
 end
--- spec/requests/tickets_copybara_spec.rb
+++ spec/requests/tickets_copybara_spec.rb
@@ -38,94 +38,135 @@ describe 'Ticket' do
       end
       include_context 'tickets page'
     end
+
+    describe 'search on unique code' do
+      before do
+        fill_in :term, with: ticket.uniques_code
+        click_button 'Search'
+      end
+      specify 'redirect to ticket' do
+        expect(current_path).to eq ticket_path(ticket)
+
+        assert_selector('li.filter > a.list-group-item', count: 0)
+        assert_selector("a[href='#{tickets_path}']", text: 'All Tickets', count: 1)
+        assert_selector("a[href='#{tickets_path}']", text: 'Back', count: 1)
+
+        visit edit_ticket_path(ticket)
+        assert_selector('li.filter > a.list-group-item', count: 0)
+        assert_selector("a[href='#{tickets_path}']", text: 'All Tickets', count: 1)
+        assert_selector("a[href='#{tickets_path}']", text: 'Back', count: 1)
+      end
+
+    end
   end
 
   describe 'four views', js: true do
-
-    let(:completed) { Status.find_by_name('Completed').id }
-    let(:on_hold)   { Status.find_by_name('On Hold').id }
-    let(:closed)    { Status.find_by_name('Completed').id }
-
     before do
-      FactoryGirl.create(:ticket, subject: 'optional subj', content: 'temporary', client_id: Client.first.id)
-      FactoryGirl.create(:ticket, subject: 'optional22',    content: 'temporary222', client_id: Client.first.id)
-      FactoryGirl.create(:ticket, subject: 'optional3',     content: 'temp orar y 3', client_id: Client.first.id)
+      on_hold_id = Status.find_by_name('On Hold').id
+      closed_id = Status.find_by_name('Completed').id
 
-      FactoryGirl.create(:ticket, subject: 'closed 1',   status_id: completed, client_id: Client.first.id)
-      FactoryGirl.create(:ticket, subject: 'closed 123', status_id: completed, client_id: Client.first.id)
+      FactoryGirl.create(:ticket, subject: 'optional subj', content: 'temporary')
+      FactoryGirl.create(:ticket, subject: 'optional22',    content: 'temporary222')
+      FactoryGirl.create(:ticket, subject: 'optional3',     content: 'temp orar y 3')
 
-      FactoryGirl.create(:ticket, subject: 'on hold',   status_id: on_hold, client_id: Client.first.id)
-      FactoryGirl.create(:ticket, subject: 'on hold22', status_id: on_hold, client_id: Client.first.id)
+      FactoryGirl.create(:ticket, subject: 'closed 1',   status_id: closed_id)
+      FactoryGirl.create(:ticket, subject: 'closed 123', status_id: closed_id)
 
-      FactoryGirl.create(:ticket, subject: 'closed 1212', staff_member_id: staffmember.id, client_id: Client.first.id)
+      FactoryGirl.create(:ticket, subject: 'on hold',   status_id: on_hold_id)
+      FactoryGirl.create(:ticket, subject: 'on hold22', status_id: on_hold_id)
+      FactoryGirl.create(:ticket, subject: 'onhold221', status_id: on_hold_id)
+
+      FactoryGirl.create(:ticket, subject: 'closed 1212', staff_member_id: staffmember.id)
 
       visit tickets_path
     end
 
-    let(:new_unassigned) { Ticket.unassigned_open_tickets.count }
-    let(:open_ticket)    { Ticket.all_open_tickets.count }
-    let(:on_hold)        { Ticket.on_hold_tickets.count }
-    let(:closed)         { Ticket.completed_tickets.count }
+    let(:new_unassigned_count) { Ticket.unassigned_open_tickets.count }
+    let(:open_ticket_count)    { Ticket.all_open_tickets.count }
+    let(:on_hold_count)        { Ticket.on_hold_tickets.count }
+    let(:closed_count)         { Ticket.completed_tickets.count }
 
     it 'New unassigned tickets' do
-      assert_selector('a.list-group-item', text: 'New unassigned tickets', count: 1)
+      assert_selector('a.list-group-item', text: 'New unassigned tickets',
+                                           count: 1)
       click_link 'New unassigned tickets'
-      sleep 1
 
-      assert_selector('div#header-tickets > h1',     text: 'New unassigned tickets', count: 1)
-      assert_selector('a.list-group-item.is-active', text: 'New unassigned tickets', count: 1)
-      assert_selector('a.list-group-item',           text: 'Open tickets',           count: 1)
-      assert_selector('a.list-group-item',           text: 'On hold tickets',        count: 1)
-      assert_selector('a.list-group-item',           text: 'Closed tickets',         count: 1)
-      assert_selector('ul.nav-tickets > li.filter > a.list-group-item',              count: 4)
+      assert_selector('div#header-tickets > h1', text: 'New unassigned tickets',
+                                                 count: 1)
+      assert_selector('a.list-group-item.is-active', text: 'New unassigned tickets',
+                                                     count: 1)
+      assert_selector('a.list-group-item', text: 'Open tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item', text: 'On hold tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item', text: 'Closed tickets',
+                                           count: 1)
+      assert_selector('ul.nav-tickets > li.filter > a.list-group-item',
+                                           count: 4)
 
-      assert_selector('ul.list-group > li.list-group-item > a.show_ticket', count: new_unassigned)
-      assert_selector('a', text: 'Edit', count: new_unassigned)
+      assert_selector('ul.list-group > li.list-group-item > a.show_ticket',
+                      count: new_unassigned_count)
+      assert_selector('a', text: 'Edit', count: new_unassigned_count)
     end
     it 'Open tickets' do
       assert_selector('a.list-group-item', text: 'New unassigned tickets', count: 1)
       click_link 'Open tickets'
-      sleep 1
 
-      assert_selector('div#header-tickets > h1',     text: 'Open tickets',           count: 1)
-      assert_selector('a.list-group-item',           text: 'New unassigned tickets', count: 1)
-      assert_selector('a.list-group-item.is-active', text: 'Open tickets',           count: 1)
-      assert_selector('a.list-group-item',           text: 'On hold tickets',        count: 1)
-      assert_selector('a.list-group-item',           text: 'Closed tickets',         count: 1)
-      assert_selector('ul.nav-tickets > li.filter > a.list-group-item',              count: 4)
+      assert_selector('div#header-tickets > h1', text: 'Open tickets',
+                                                 count: 1)
+      assert_selector('a.list-group-item', text: 'New unassigned tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item.is-active', text: 'Open tickets',
+                                                     count: 1)
+      assert_selector('a.list-group-item', text: 'On hold tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item', text: 'Closed tickets',
+                                           count: 1)
+      assert_selector('ul.nav-tickets > li.filter > a.list-group-item',
+                       count: 4)
 
-      assert_selector('ul.list-group > li.list-group-item > a.show_ticket', count: open_ticket)
-      assert_selector('a', text: 'Edit', count: open_ticket)
+      assert_selector('ul.list-group > li.list-group-item > a.show_ticket', count: open_ticket_count)
+      assert_selector('a', text: 'Edit', count: open_ticket_count)
     end
     it 'On hold tickets' do
       assert_selector('a.list-group-item', text: 'New unassigned tickets', count: 1)
       click_link 'On hold tickets'
-      sleep 1
 
-      assert_selector('div#header-tickets > h1',     text: 'On hold tickets',        count: 1)
-      assert_selector('a.list-group-item',           text: 'New unassigned tickets', count: 1)
-      assert_selector('a.list-group-item',           text: 'Open tickets',           count: 1)
-      assert_selector('a.list-group-item.is-active', text: 'On hold tickets',        count: 1)
-      assert_selector('a.list-group-item',           text: 'Closed tickets',         count: 1)
-      assert_selector('ul.nav-tickets > li.filter > a.list-group-item',              count: 4)
+      assert_selector('div#header-tickets > h1', text: 'On hold tickets',
+                                                 count: 1)
+      assert_selector('a.list-group-item', text: 'New unassigned tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item', text: 'Open tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item.is-active', text: 'On hold tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item', text: 'Closed tickets',
+                                           count: 1)
+      assert_selector('ul.nav-tickets > li.filter > a.list-group-item',
+                                           count: 4)
 
-      assert_selector('ul.list-group > li.list-group-item > a.show_ticket', count: on_hold)
-      assert_selector('a', text: 'Edit', count: on_hold)
+      assert_selector('ul.list-group > li.list-group-item > a.show_ticket', count: on_hold_count)
+      assert_selector('a', text: 'Edit', count: on_hold_count)
     end
     it 'Closed tickets' do
       assert_selector('a.list-group-item', text: 'New unassigned tickets', count: 1)
       click_link 'Closed tickets'
-      sleep 1
 
-      assert_selector('div#header-tickets > h1',     text: 'Closed tickets',         count: 1)
-      assert_selector('a.list-group-item',           text: 'New unassigned tickets', count: 1)
-      assert_selector('a.list-group-item',           text: 'Open tickets',           count: 1)
-      assert_selector('a.list-group-item',           text: 'On hold tickets',        count: 1)
-      assert_selector('a.list-group-item.is-active', text: 'Closed tickets',         count: 1)
-      assert_selector('ul.nav-tickets > li.filter > a.list-group-item',              count: 4)
+      assert_selector('div#header-tickets > h1', text: 'Closed tickets',
+                                                 count: 1)
+      assert_selector('a.list-group-item', text: 'New unassigned tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item', text: 'Open tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item', text: 'On hold tickets',
+                                           count: 1)
+      assert_selector('a.list-group-item.is-active', text: 'Closed tickets',
+                                           count: 1)
+      assert_selector('ul.nav-tickets > li.filter > a.list-group-item',
+                                           count: 4)
 
-      assert_selector('ul.list-group > li.list-group-item > a.show_ticket', count: closed)
-      assert_selector('a', text: 'Edit', count: closed)
+      assert_selector('ul.list-group > li.list-group-item > a.show_ticket', count: closed_count)
+      assert_selector('a', text: 'Edit', count: closed_count)
     end
   end
 end
