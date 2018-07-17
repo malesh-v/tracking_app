d719707907b6d52c6a1a95b98f05402c5696668b
Vadim <gam.vadik@gmail.com>
2018-07-17 21:17:33 +0300

    test for search

--- spec/helpers/tickets_helper.rb
+++ spec/helpers/tickets_helper.rb
@@ -3,6 +3,7 @@ require 'support/utilities'
 
 shared_context 'tickets page' do
   describe 'GET statuses list' do
+
     specify do
       click_button 'Search'
 
@@ -15,26 +16,34 @@ shared_context 'tickets page' do
       assert_selector('li.filter > a.list-group-item',      count: 0)
       assert_selector('div#header-tickets > h1', text: 'Tickets list', count: 1)
 
+      #show ticket parh
       visit ticket_path(Ticket.first)
-      expect(html_part.decoded).to have_link(url_with_term)
+      assert_selector("a[href='#{url_with_term}']", text: 'Back', count: 1)
 
-      debugger
-      find(:xpath, "//a[@href=#{url_with_term}]")
-
+      #edit ticket path
+      visit edit_ticket_path(Ticket.first)
+      assert_selector("a[href='#{url_with_term}']", text: 'Back', count: 1)
     end
     specify do
-
+      url_with_term = tickets_path(term: "#{cookies[:term]}")
       click_button 'Search'
 
-      find(:xpath, "//a[@href='/tickets?no_term=true']").click
+      find(:xpath, "//a[@href='/tickets?no_term=true']")
 
       #tickets path
       expect(current_path).to eq tickets_path
-      assert_selector('li.filter > a.list-group-item',                 count: 4)
+      assert_selector('li.list-group-item > a.show_ticket', count: 2)
+      assert_selector('a.badge', text: 'Edit',              count: 2)
+      assert_selector('li.filter > a.list-group-item',      count: 0)
       assert_selector('div#header-tickets > h1', text: 'Tickets list', count: 1)
 
-      visit edit_ticket_path(ticket)
+      #show ticket parh
+      visit ticket_path(Ticket.first)
+      assert_selector("a[href='#{url_with_term}']", text: 'Back', count: 1)
 
+      #edit ticket path
+      visit edit_ticket_path(Ticket.first)
+      assert_selector("a[href='#{url_with_term}']", text: 'Back', count: 1)
     end
   end
 end
